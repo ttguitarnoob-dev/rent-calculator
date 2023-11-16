145ba6eb230d29436a94a21a3e08771afffa9d19
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-11-16 15:49:06 -0600

    initial styling

--- src/App.js
+++ src/App.js
@@ -11,47 +11,53 @@ function App() {
 
   const [item, setItem] = useState()
 
-    async function handleFetch() {
-        URL = "https://api.ttguitarnoob.cloud/rents/6554cb7a60c180217c7a8198"
-        const options = {
-            method: "GET"
-        }
-
-        try {
-            const responst = await fetch(URL)
-            const data = await responst.json()
-            setItem(data)
-            console.log('data fetched', data)
-        } catch(err) {
-            console.log('something terrible happened when fetching', err)
-        }
-            
-        
+  async function handleFetch() {
+    URL = "https://api.ttguitarnoob.cloud/rents/6554cb7a60c180217c7a8198"
+    const options = {
+      method: "GET"
     }
 
-    useEffect(() => {
-        handleFetch()
-    }, [])
-
-    function setTheState(data) {
+    try {
+      const responst = await fetch(URL)
+      const data = await responst.json()
       setItem(data)
-      console.log('the data', data)
+      console.log('data fetched', data)
+    } catch (err) {
+      console.log('something terrible happened when fetching', err)
     }
 
-    if (!item) return <div>
-        <CircularProgress label="Loading..." />
-    </div>
+
+  }
+
+  useEffect(() => {
+    handleFetch()
+  }, [])
+
+  function setTheState(data) {
+    setItem(data)
+    console.log('the data', data)
+  }
+
+  if (!item) return <div>
+    <CircularProgress label="Loading..." />
+  </div>
 
 
   return (
-<NextUIProvider>
-    <div className="App dark text-foreground bg-background">
-      <Owe data={item} />
-      <Paid data={item} />
-      <Total data={item} />
-      <AddItem data={item} setItem={setTheState} />
-    </div>
-</NextUIProvider>
+    <NextUIProvider>
+      <div className="App dark text-foreground bg-background">
+        <section className='data-section'>
+          <div className='data-display'>
+            <Owe data={item} />
+          </div>
+          <div className='data-display'>
+            <Paid data={item} />
+          </div>
+        </section>
+        <Total data={item} />
+        <AddItem data={item} setItem={setTheState} />
+      </div>
+    </NextUIProvider>
   );
 }
 
